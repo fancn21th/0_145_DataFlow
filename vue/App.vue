b1a70a0b238fd1ab0f1ba0872df3a5e2e7d4a098
范轶洁 <fanyjb@digitalchina.com>
2020-09-20 09:57:37 +0800

    children 渲染

--- vue/App.vue
+++ vue/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
     <Foo text="hello foo" />
+    <hr />
     <Bar />
+    <hr />
+    <List :contacts="contacts" />
   </div>
 </template>
 
 <script>
 import Foo from "./components/Foo";
 import Bar from "./components/Bar";
+import List from "./components/List";
 
 export default {
   components: {
     Foo,
     Bar,
+    List,
+  },
+  data() {
+    return {
+      contacts: [
+        {
+          name: { first: "轶洁", last: "范" },
+        },
+        {
+          name: { first: "超", last: "李" },
+        },
+        {
+          name: { first: "胜", last: "郭" },
+        },
+        {
+          name: { first: "军", last: "彭" },
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ vue/components/List.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  props: {
+    contacts: {
+      type: Array,
+      default: [],
+    },
+  },
+  render(h) {
+    return h(
+      "ul",
+      {
+        attrs: {
+          class: "name-list",
+        },
+      },
+      this.contacts.map((contact) => {
+        return h(
+          "li",
+          {
+            attrs: {
+              class: "name",
+            },
+          },
+          `${contact.name.first} ${contact.name.last}`
+        );
+      })
+    );
+  },
+};
+</script>
+
+<style scoped>
+.name-list {
+  padding-left: 0;
+}
+.name {
+  background-color: #efefef;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.name:hover {
+  background-color: #ddd;
+}
+</style>
